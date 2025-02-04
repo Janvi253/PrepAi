65597f901f972c5df313ec3434466e1468763d78
ku28 <ku28sh07@gmail.com>
2025-02-04 22:46:32 +0530

    Refactor header component

--- app/dashboard/_components/header.jsx
+++ app/dashboard/_components/header.jsx
@@ -1,11 +1,20 @@
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
 import React from 'react'
 
-function header() {
+function Header() {
   return (
-    <div>
-        <Image src={'/logo.svg'} width={160} height={100} alt='logo'/>
+    <div className='flex p-4 items-center justify-between bg-secondary shadow-md'>
+        <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
+        <ul className='flex gap-6'>
+          <li>Dashboard</li>
+          <li>Questions</li>
+          <li>Upgrade</li>
+          <li>How it Works?</li>
+        </ul>
+        <UserButton/>
     </div>
   )
 }
 
-export default header
+export default Header
