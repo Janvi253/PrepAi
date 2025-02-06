fbd1caafe2ff2b08e99141ac1a6ad3237723131d
ku28 <ku28sh07@gmail.com>
2025-02-06 21:48:02 +0530

    Add database utility

--- app/dashboard/_components/header.jsx
+++ app/dashboard/_components/header.jsx
@@ -12,11 +12,20 @@ function Header() {
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-md'>
         <Image src={'/logo.svg'} width={160} height={100} alt='logo' />
-        <ul className='flex gap-6'>
-          <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Dashboard</li>
-          <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Questions</li>
-          <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>Upgrade</li>
-          <li className='hover:text-primary hover:font-bold transition-all cursor-pointer'>How it Works?</li>
+        <ul className='hidden md:flex gap-6'>
+          <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path=='/dashboard' && 'text-primary'}
+          `}
+          >Dashboard</li>
+          <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path=='/dashboard/questions' && 'text-primary'}
+          `}>Questions</li>
+          <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path=='/dashboard/upgrade' && 'text-primary'}
+          `}>Upgrade</li>
+          <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path=='/dashboard/how' && 'text-primary'}
+          `}>How it Works?</li>
         </ul>
         <UserButton/>
     </div>
